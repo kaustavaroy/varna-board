8cec0208f86a2b81c635d9eaf4d564c326183637
kaustavaroy <kaustava.roy@gmail.com>
2025-08-29 16:11:39 -0700

    Update lekhni_v3.js

--- lekhni_v3.js
+++ lekhni_v3.js
@@ -84,8 +84,8 @@ $vkey.innerHTML = `
     background-color: rgba(0,0,0,0.2);
     border-radius: 50%;
     position: relative;
-    left: 6px;
-    top: -30px;
+    left: 18px;
+    top: -42px;
     text-align: center;
     line-height: 24px; /* to vertically center text; set to height*/
   }
